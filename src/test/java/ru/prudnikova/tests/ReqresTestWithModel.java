152d1eb407f2e5239f1fb17bbdce6d30d9eadbc8
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-04-29 19:14:11 +0300

    add

--- src/test/java/ru/prudnikova/tests/ReqresTestWithModel.java
+++ src/test/java/ru/prudnikova/tests/ReqresTestWithModel.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
-import static ru.prudnikova.generators.helpers.CustomAllureListener.withCustomTemplates;
 import static ru.prudnikova.specs.Specs.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -21,77 +20,83 @@ public class ReqresTestWithModel {
 
     @Test
     @Tag("Api")
-    void getUserDataFromPage2() {
-        UserData data = step("Make request", () ->given()
+    void getUserData() {
+        UserData data = step("Make request to get list of users from page 2", () -> given()
                 .spec(requestSpec)
                 .when()
                 .get("/users?page=2")
                 .then()
                 .spec(responseSpec200).extract().as(UserData.class));
-        step("Verify id", () ->
-        assertThat(data.getUser().stream().map(el -> el.getId()).collect(Collectors.toList()), hasItems(7, 8, 9, 10, 11, 12)));
-        step("Verify email", () ->
-        assertThat(data.getUser().stream().map(User::getEmail).collect(Collectors.toList()), everyItem(endsWith("@reqres.in"))));
-        step("Verify firstName", () ->
-        assertThat(data.getUser().stream().map(User::getFirstName).collect(Collectors.toList()), hasItems("Michael", "Lindsay", "Tobias", "Byron", "George", "Rachel")));
-        step("Verify lastName", () ->
-        assertThat(data.getUser().stream().map(User::getLastName).collect(Collectors.toList()), hasItems("Lawson", "Ferguson", "Funke", "Fields", "Edwards", "Howell")));
-        step("Verify avatar", () ->
-        assertThat(data.getUser().stream().map(User::getAvatar).collect(Collectors.toList()), hasItem("https://reqres.in/img/faces/7-image.jpg")));
+        step("Verify users id ", () ->
+                assertThat(data.getUser().stream().map(el -> el.getId()).collect(Collectors.toList()), hasItems(7, 8, 9, 10, 11, 12)));
+        step("Verify users email", () ->
+                assertThat(data.getUser().stream().map(User::getEmail).collect(Collectors.toList()), everyItem(endsWith("@reqres.in"))));
+        step("Verify users firstName", () ->
+                assertThat(data.getUser().stream().map(User::getFirstName).collect(Collectors.toList()), hasItems("Michael", "Lindsay", "Tobias", "Byron", "George", "Rachel")));
+        step("Verify users lastName", () ->
+                assertThat(data.getUser().stream().map(User::getLastName).collect(Collectors.toList()), hasItems("Lawson", "Ferguson", "Funke", "Fields", "Edwards", "Howell")));
+        step("Verify users avatar", () ->
+                assertThat(data.getUser().stream().map(User::getAvatar).collect(Collectors.toList()), hasItem("https://reqres.in/img/faces/7-image.jpg")));
     }
 
     @Test
     @Tag("Api")
-    void checkSingleResourceNotFound() {
-        given()
+    void getUnknownUser() {
+        step("Make request to to get unknown user", () -> given()
                 .spec(requestSpec)
                 .when()
                 .get("/unknown/23")
                 .then()
                 .spec(responseSpec)
-                .statusCode(404);
+                .statusCode(404));
     }
 
     @Test
     @Tag("Api")
     void createUser() {
-        CreateUserModel userModel = CreateUserModelGenerator.generationUserCreateBody();
-        given()
+        CreateUserModel body = CreateUserModelGenerator.generationUserCreateBody();
+        CreateUserModel user = step("Make request to create user", () -> given()
                 .spec(requestSpec)
-                .body(userModel)
+                .body(body)
                 .when()
                 .post("/users")
                 .then()
                 .spec(responseSpec)
-                .statusCode(201)
-                .body("name", is(userModel.getFullName()), "job", is(userModel.getJob()));
+                .statusCode(201).extract().as(CreateUserModel.class));
+        step("Verify user fullName", () ->
+                assertThat(user.getFullName(), is(body.getFullName())));
+        step("Verify user job", () ->
+                assertThat(user.getJob(), is(body.getJob())));
     }
 
     @Test
     @Tag("Api")
     void updateUserId2() {
-        CreateUserModel userModel = CreateUserModelGenerator.generationUserCreateBody();
-        given()
+        CreateUserModel body = CreateUserModelGenerator.generationUserCreateBody();
+        CreateUserModel user = step("Make request to update user", () -> given()
                 .spec(requestSpec)
-                .body(userModel)
+                .body(body)
                 .when()
                 .put("/users/2")
                 .then()
-                .spec(responseSpec200)
-                .body("name", is(userModel.getFullName()), "job", is(userModel.getJob()));
+                .spec(responseSpec200).extract().as(CreateUserModel.class));
+        step("Verify user fullName", () ->
+                assertThat(user.getFullName(), is(body.getFullName())));
+        step("Verify user job", () ->
+                assertThat(user.getJob(), is(body.getJob())));
     }
 
     @Test
     @Tag("Api")
-    void deleteUser2() {
-        given()
+    void deleteUser() {
+        step("Make request to to delete user with id 2", () -> given()
                 .spec(requestSpec)
                 .when()
                 .delete("/users/2")
                 .then()
                 .spec(responseSpec)
                 .statusCode(204)
-                .body(Matchers.anything());
+                .body(Matchers.anything()));
 
     }
 
